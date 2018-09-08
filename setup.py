2c6bafdb01bc4894104f049416fe6e7a62bbb1ce
Michał Wieluński <michal@w-ski.dev>
2018-09-08 22:45:08 +0200

    update README.md

--- setup.py
+++ setup.py
@@ -1,13 +1,29 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='pi3-switch',
-    version='0.0.1',
+    description='This simple program will switch to the given i3 workspace '
+                'with slightly different behavior if there are two outputs.',
+    long_description=long_description,
+    version='0.1.0',
     license='MIT',
-    author='Michał "landmaj" Wieluński',
+    author='Michał Wieluński',
     author_email='michal@landmaj.pl',
+    url='https://gitlab.com/landmaj/pi3-switch',
     install_requires=['i3ipc==1.5.1'],
+    packages=['pi3'],
+    zip_safe=True,
     entry_points={
         'console_scripts': ['pi3-switch = pi3.switch:main']
-    }
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Topic :: Desktop Environment :: Window Managers',
+      ],
 )
