3853c24d589b1a8466076640d786c33b9c9c6e46
Michał Wieluński <michal@w-ski.dev>
2018-09-08 19:11:10 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='pi3-switch',
+    version='0.0.1',
+    license='MIT',
+    author='Michał "landmaj" Wieluński',
+    author_email='michal@landmaj.pl',
+    install_requires=['i3ipc==1.5.1']
+)
